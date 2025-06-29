0dc2756640496b0cee295bfc41c43e525c291454
JuanVimon3 <jvillamonsalve@gmail.com>
2025-06-29 11:36:30 -0500

    change header title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="bg-gray-800 text-white shadow-md">
       <div className="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
-        <h1 className="text-xl font-bold"><Link href="/" className="hover:underline">Mi Tienda</Link></h1>
+        <h1 className="text-xl font-bold"><Link href="/" className="hover:underline">VÉLEZ</Link></h1>
         <nav className="space-x-4">
           <Link href="/" className="hover:underline">Inicio</Link>
           {/* <Link href="/carrito" className="hover:underline">Carrito</Link> */}
